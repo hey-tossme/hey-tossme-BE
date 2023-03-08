52bbcc345452b962a12ec37fac6cfa2b10e6b02f
DESKTOP-G85SRSM\hwlsg <hjhlove1120@gmail.com>
2023-03-08 23:35:05 +0900

    feat: temp 브랜치 생성 및 코드 리팩토링
    1. 브랜치 생성
    2. item(entity) import 수정
    3. 코드에디터 적용

--- src/main/java/com/blackdragon/heytossme/persist/entity/Address.java
+++ src/main/java/com/blackdragon/heytossme/persist/entity/Address.java
@@ -1,6 +1,5 @@
 package com.blackdragon.heytossme.persist.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
