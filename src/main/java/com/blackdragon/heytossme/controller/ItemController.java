4bd29786e4a8e9368484b5a4c7f4d4789ac9c004
DESKTOP-G85SRSM\hwlsg <hjhlove1120@gmail.com>
2023-03-10 20:54:35 +0900

    feat: 아이템 등록 기능 구현
    1. 외부 api 사용하여 주소를 좌표로 변환하는 기능 구현
    2. 아이템 및 주소 테이블 등록 기능 구현

--- src/main/java/com/blackdragon/heytossme/controller/ItemController.java
+++ src/main/java/com/blackdragon/heytossme/controller/ItemController.java
@@ -3,6 +3,7 @@ package com.blackdragon.heytossme.controller;
 import com.blackdragon.heytossme.dto.ItemDto.CreateItemRequest;
 import com.blackdragon.heytossme.dto.ResponseForm;
 import com.blackdragon.heytossme.service.ItemService;
+import com.blackdragon.heytossme.type.ItemResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,8 +19,8 @@ public class ItemController {
     private final ItemService itemService;
 
     @PostMapping
-    public ResponseEntity<ResponseForm> test(@RequestBody CreateItemRequest request) {
-        itemService.createItem(request);
-        return null;
+    public ResponseEntity<ResponseForm> registerItem(@RequestBody CreateItemRequest request) {
+        var data = itemService.createItem(request);
+        return ResponseEntity.ok(new ResponseForm(ItemResponse.REGISTER_ITEM.getMessage(), data));
     }
 }
