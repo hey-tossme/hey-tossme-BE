1d812795a4bf9698a2745913e5a9c1e27a73f3d6
bearjun05 <bearjun05@naver.com>
2023-03-11 23:41:57 +0900

    feat: ExceptionResponse 추가

---
+++ src/main/java/com/blackdragon/heytossme/exception/ExceptionResponse.java
@@ -0,0 +1,18 @@
+package com.blackdragon.heytossme.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ExceptionResponse {
+
+    private String message;
+
+}
