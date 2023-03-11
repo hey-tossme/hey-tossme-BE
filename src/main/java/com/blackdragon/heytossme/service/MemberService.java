1bc73b6d019a06de1ef9cee70425888f76a981fc
bearjun05 <bearjun05@naver.com>
2023-03-11 23:53:03 +0900

    feat: 유저 수정 기능 추가

--- src/main/java/com/blackdragon/heytossme/service/MemberService.java
+++ src/main/java/com/blackdragon/heytossme/service/MemberService.java
@@ -3,15 +3,18 @@ package com.blackdragon.heytossme.service;
 import static com.blackdragon.heytossme.exception.MemberErrorCode.NOT_FOUND_USER;
 
 import com.blackdragon.heytossme.dto.MemberDto;
+import com.blackdragon.heytossme.dto.MemberDto.ModifyRequest;
 import com.blackdragon.heytossme.dto.MemberDto.Response;
 import com.blackdragon.heytossme.dto.MemberDto.SignUpRequest;
 import com.blackdragon.heytossme.exception.CustomException;
 import com.blackdragon.heytossme.exception.ErrorCode;
+import com.blackdragon.heytossme.exception.MemberErrorCode;
 import com.blackdragon.heytossme.exception.MemberException;
 import com.blackdragon.heytossme.persist.MemberRepository;
 import com.blackdragon.heytossme.persist.entity.Member;
 import com.blackdragon.heytossme.type.MemberStatus;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,6 +22,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -51,6 +55,44 @@ public class MemberService {
                 .orElseThrow(() -> new MemberException(NOT_FOUND_USER));
 
         Response response = modelMapper.map(member, Response.class);
+
+        return response;
+    }
+
+    public Response modifyInfo(long userId, ModifyRequest request) {
+
+        Member member = memberRepository.findById(userId)
+                .orElseThrow(() -> new MemberException(NOT_FOUND_USER));
+
+        log.info(passwordEncoder.encode(request.getCurPassword()));
+        // 사용자 비밀번호 확인
+        if (!passwordEncoder.matches(request.getCurPassword(), member.getPassword())) {
+            throw new MemberException(MemberErrorCode.INCORRECT_PASSWORD);
+        }
+        // 변경 후 비밀번호가 변경 전 비밀번호와 같을때
+        if (passwordEncoder.encode(request.getPassword()).equals(member.getPassword())) {
+            throw new MemberException(MemberErrorCode.MATCH_PREVIOUS_PASSWORD);
+        }
+
+        Member updatedMember = Member.builder()
+                .id(member.getId())
+                .email(request.getEmail() != null ? request.getEmail() : member.getEmail())
+                .name(request.getName() != null ? request.getName() : member.getName())
+                .password(request.getPassword() != null ?
+                        passwordEncoder.encode(request.getPassword()) : member.getPassword())
+                .imageUrl(request.getImageUrl() != null ? request.getImageUrl()
+                        : member.getImageUrl())
+                .socialLoginType(request.getSocialType() != null ? request.getSocialType()
+                        : member.getSocialLoginType())
+                .account(request.getAccount() != null ? request.getAccount() : member.getAccount())
+                .bankName(request.getBankName() != null ? request.getBankName()
+                        : member.getBankName())
+                .status(member.getStatus())
+                .build();
+
+        Member save = memberRepository.save(updatedMember);
+
+        Response response = modelMapper.map(updatedMember, Response.class);
 
         return response;
     }
