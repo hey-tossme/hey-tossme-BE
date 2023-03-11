0db67f9a1f7204e5dd81dbaf0e4cc889bf0d1758
bearjun05 <bearjun05@naver.com>
2023-03-11 23:51:51 +0900

    feat: 유저 삭제 기능 추가

--- src/main/java/com/blackdragon/heytossme/controller/MemberController.java
+++ src/main/java/com/blackdragon/heytossme/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.blackdragon.heytossme.controller;
 
+import com.blackdragon.heytossme.dto.MemberDto.ModifyRequest;
 import com.blackdragon.heytossme.dto.MemberDto.Response;
 import com.blackdragon.heytossme.dto.MemberDto.SignUpRequest;
 import com.blackdragon.heytossme.dto.ResponseForm;
@@ -8,7 +9,9 @@ import com.blackdragon.heytossme.type.MemberResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,4 +41,21 @@ public class MemberController {
                 new ResponseForm(MemberResponse.FIND_INFO.getMessage(), response));
     }
 
+    @PatchMapping
+    public ResponseEntity<ResponseForm> modifyInfo(@RequestBody ModifyRequest request) {
+
+        Response response = memberService.modifyInfo(4L, request);
+
+        return ResponseEntity.ok(
+                new ResponseForm(MemberResponse.CHANGE_INFO.getMessage(), response));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<ResponseForm> delete(@RequestBody ModifyRequest request) {
+
+        Response response = memberService.deleteUser(2L, request);
+
+        return ResponseEntity.ok(
+                new ResponseForm(MemberResponse.DELETE_USER.getMessage(), response));
+    }
 }
