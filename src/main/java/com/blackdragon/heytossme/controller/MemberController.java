e849b9776241ffd2fd8377d11b7ec8422b77e053
bearjun05 <bearjun05@naver.com>
2023-03-11 23:49:00 +0900

    feat: 사용자 정보 조회 기능 추가

--- src/main/java/com/blackdragon/heytossme/controller/MemberController.java
+++ src/main/java/com/blackdragon/heytossme/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.blackdragon.heytossme.controller;
 
+import com.blackdragon.heytossme.dto.MemberDto.Response;
 import com.blackdragon.heytossme.dto.MemberDto.SignUpRequest;
 import com.blackdragon.heytossme.dto.ResponseForm;
 import com.blackdragon.heytossme.service.MemberService;
@@ -7,6 +8,7 @@ import com.blackdragon.heytossme.type.MemberResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +25,17 @@ public class MemberController {
     @PostMapping
     public ResponseEntity<ResponseForm> signUp(@RequestBody SignUpRequest request) {
         var data = memberService.signUp(request);
-        return ResponseEntity.ok(new ResponseForm(MemberResponse.SING_UP.getMessage(), data));
+        return ResponseEntity.ok(new ResponseForm(MemberResponse.SIGN_UP.getMessage(), data));
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseForm> getInfo() {
+        //Object userId = request.getAttribute("id");
+
+        Response response = memberService.getInfo(3L);
+
+        return ResponseEntity.ok(
+                new ResponseForm(MemberResponse.FIND_INFO.getMessage(), response));
     }
 
 }
