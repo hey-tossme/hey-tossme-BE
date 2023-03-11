0310197d615af5996fa1856aa2ce86ead1a32022
bearjun05 <bearjun05@naver.com>
2023-03-11 23:45:19 +0900

    feat: 유저 정보 조회 기능 추가

--- src/main/java/com/blackdragon/heytossme/service/MemberService.java
+++ src/main/java/com/blackdragon/heytossme/service/MemberService.java
@@ -1,14 +1,18 @@
 package com.blackdragon.heytossme.service;
 
+import static com.blackdragon.heytossme.exception.MemberErrorCode.NOT_FOUND_USER;
+
 import com.blackdragon.heytossme.dto.MemberDto;
 import com.blackdragon.heytossme.dto.MemberDto.Response;
 import com.blackdragon.heytossme.dto.MemberDto.SignUpRequest;
 import com.blackdragon.heytossme.exception.CustomException;
 import com.blackdragon.heytossme.exception.ErrorCode;
+import com.blackdragon.heytossme.exception.MemberException;
 import com.blackdragon.heytossme.persist.MemberRepository;
 import com.blackdragon.heytossme.persist.entity.Member;
 import com.blackdragon.heytossme.type.MemberStatus;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -19,6 +23,7 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    private final ModelMapper modelMapper;
 
     public MemberDto.Response signUp(SignUpRequest request) {
 
@@ -38,6 +43,16 @@ public class MemberService {
                         .build()
         );
         return new Response(member);
+    }
+
+    public Response getInfo(long userId) {
+
+        Member member = memberRepository.findById(userId)
+                .orElseThrow(() -> new MemberException(NOT_FOUND_USER));
+
+        Response response = modelMapper.map(member, Response.class);
+
+        return response;
     }
 
 
