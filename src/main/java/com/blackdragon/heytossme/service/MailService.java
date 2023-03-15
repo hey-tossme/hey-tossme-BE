ac7c9b422798a594ca24a29f9751be60bee496e3
bearjun05 <bearjun05@naver.com>
2023-03-16 00:08:44 +0900

    feat: 메일 메시지 생성 기능 추가

--- src/main/java/com/blackdragon/heytossme/service/MailService.java
+++ src/main/java/com/blackdragon/heytossme/service/MailService.java
@@ -38,4 +38,35 @@ public class MailService {
             throw new RuntimeException("인증번호 불일치");
         }
     }
+    private MimeMessage createMessage(String email) {
+        System.out.println("보내는 대상 : " + email);
+        System.out.println("인증 번호 : " + email);
+        MimeMessage message = mailSender.createMimeMessage();
+        try {
+            message.addRecipients(RecipientType.TO, email);//보내는 대상
+            message.setSubject("이메일 인증 테스트");//제목
+            String msg = "";
+            msg += "<div style='margin:20px;'>";
+            msg += "<h1> 안녕하세요 hey-toss-me 입니다. </h1>";
+            msg += "<br>";
+            msg += "<p>아래 인증코드를 복사해 입력해주세요<p>";
+            msg += "<br>";
+            msg += "<p>감사합니다.<p>";
+            msg += "<br>";
+            msg += "<div align='center' style='border:1px solid black; font-family:verdana';>";
+            msg += "<h3 style='color:blue;'>회원가입 인증 코드입니다.</h3>";
+            msg += "<div style='font-size:130%'>";
+            msg += "CODE : <strong>";
+            msg += emailAuthKey + "</strong><div><br/> ";
+            msg += "</div>";
+            message.setText(msg, "utf-8", "html");//내용
+            message.setFrom(new InternetAddress("heytossme@gmail.com", "heytossme"));//보내는 사람
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+        return message;
+    }
 }
