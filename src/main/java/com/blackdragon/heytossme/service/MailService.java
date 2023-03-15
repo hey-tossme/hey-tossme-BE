fc31771829b4d839bf0d6264a16893e2ed38ca4f
bearjun05 <bearjun05@naver.com>
2023-03-16 00:11:56 +0900

    feat: 메일 중복, 메일 유효성 검사 기능 추가

--- src/main/java/com/blackdragon/heytossme/service/MailService.java
+++ src/main/java/com/blackdragon/heytossme/service/MailService.java
@@ -24,9 +24,10 @@ public class MailService {
     private final MailRepository mailRepository;
     private final String emailAuthKey = createKey();
 
+
     public void send(MailDto mailDto) {
         log.info(mailDto.getEmail());
-        validate(mailDto.getEmail());
+        validateEmail(mailDto.getEmail());
         checkDuplicate(mailDto.getEmail());
         MimeMessage message = createMessage(mailDto.getEmail());
         mailSender.send(message);
@@ -34,7 +35,7 @@ public class MailService {
     public void validate(MailDto mailDto) {
         EmailCertification emailCertification = mailRepository.findById(mailDto.getEmail())
                 .orElseThrow(() -> new RuntimeException("이메일이 없습니다"));
-        if (!emailCertification.getEmailAuthKey().equals(mailDto.getCode())) {
+        if(!emailCertification.getEmailAuthKey().equals(mailDto.getCode())){
             throw new RuntimeException("인증번호 불일치");
         }
     }
@@ -44,7 +45,9 @@ public class MailService {
         MimeMessage message = mailSender.createMimeMessage();
         try {
             message.addRecipients(RecipientType.TO, email);//보내는 대상
+
             message.setSubject("이메일 인증 테스트");//제목
+
             String msg = "";
             msg += "<div style='margin:20px;'>";
             msg += "<h1> 안녕하세요 hey-toss-me 입니다. </h1>";
@@ -68,5 +71,47 @@ public class MailService {
             throw new RuntimeException(e);
         }
         return message;
+    }
+    private String createKey() {
+        StringBuffer key = new StringBuffer();
+        Random rnd = new Random();
+
+        for (int i = 0; i < 8; i++) { // 인증코드 8자리
+            int index = rnd.nextInt(3); // 0~2 까지 랜덤
+
+            switch (index) {
+                case 0:
+                    key.append((char) ((int) (rnd.nextInt(26)) + 97));
+                    //  a~z  (ex. 1+97=98 => (char)98 = 'b')
+                    break;
+                case 1:
+                    key.append((char) ((int) (rnd.nextInt(26)) + 65));
+                    //  A~Z
+                    break;
+                case 2:
+                    key.append((rnd.nextInt(10)));
+                    // 0~9
+                    break;
+            }
+        }
+        return key.toString();
+    }
+
+    private EmailCertification checkDuplicate(String email) {
+        mailRepository.findById(email).ifPresent(i -> {
+            throw new RuntimeException("중복된 이메일");
+        });
+        return mailRepository.save(EmailCertification.builder()
+                .email(email)
+                .emailAuthKey(emailAuthKey)
+                .build());
+    }
+    private void validateEmail(String email) {
+        try {
+            InternetAddress emailAddress = new InternetAddress(email);
+            emailAddress.validate();
+        } catch (AddressException e) {
+            throw new IllegalArgumentException("유효하지 않은 이메일 주소입니다: " + email, e);
+        }
     }
 }
