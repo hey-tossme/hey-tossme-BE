f98a680d46be8d67d83db88caba1dd1daa695c79
bearjun05 <bearjun05@naver.com>
2023-03-16 15:06:43 +0900

    feat: 이메일로 사용자 정보 찾기 jpa 기능 추가

--- src/main/java/com/blackdragon/heytossme/persist/MemberRepository.java
+++ src/main/java/com/blackdragon/heytossme/persist/MemberRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     boolean existsByEmail(String email);
+    Member findByEmail(String email);
 }
