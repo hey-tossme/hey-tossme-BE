a22d20713f2c223e956e4374298853bfbd58edf0
bearjun05 <bearjun05@naver.com>
2023-03-16 15:05:21 +0900

    feat: kakao 로그인 기능 추가

---
+++ src/main/java/com/blackdragon/heytossme/controller/KakaoLoginController.java
@@ -0,0 +1,30 @@
+package com.blackdragon.heytossme.controller;
+
+import com.blackdragon.heytossme.service.KakaoLoginService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kakao")
+@RequiredArgsConstructor
+@Log4j2
+public class KakaoLoginController {
+
+    private final String redirectUrl = "http://localhost:8080/login/oauth2/kakao";
+    private final String clientId = "5e7a808a1af99e1088e5d4eb40239800";
+    private final KakaoLoginService kakaoLoginService;
+
+    @GetMapping("/login")
+    public String kakaoLogin() {
+
+        String url = "https://kauth.kakao.com/oauth/authorize?client_id=" + clientId
+                + "&redirect_uri=" + redirectUrl +
+                "&response_type=code";
+
+        return url;
+    }
+}
