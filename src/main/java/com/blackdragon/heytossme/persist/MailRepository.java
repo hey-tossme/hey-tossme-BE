6186c057ff64ca30e68256707ca1aa2f76bac304
bearjun05 <bearjun05@naver.com>
2023-03-21 08:19:02 +0900

    refactor: Mail 이름변경

--- src/main/java/com/blackdragon/heytossme/persist/MailRepository.java
+++ src/main/java/com/blackdragon/heytossme/persist/MailRepository.java
@@ -1,10 +1,10 @@
 package com.blackdragon.heytossme.persist;
 
-import com.blackdragon.heytossme.persist.entity.EmailCertification;
+import com.blackdragon.heytossme.persist.entity.Mail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MailRepository extends JpaRepository<EmailCertification, String> {
+public interface MailRepository extends JpaRepository<Mail, String> {
 
 }
