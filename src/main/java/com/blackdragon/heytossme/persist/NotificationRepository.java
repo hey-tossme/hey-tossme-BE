54be3c8ff6872d7dad857016f519ebb83592b1b3
bomilee <bomvll@naver.com>
2023-03-21 10:55:32 +0900

    feat: 알림기능 추가 및 알림테스트

--- src/main/java/com/blackdragon/heytossme/persist/NotificationRepository.java
+++ src/main/java/com/blackdragon/heytossme/persist/NotificationRepository.java
@@ -7,8 +7,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
-
-//	List<Notification> findAllByReadOrNotIsTrue();
-//	List<Notification> findAllByReadOrNotIsFalse();
-
 }
