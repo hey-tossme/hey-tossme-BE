6decfeb34eb562146950a21bb3dbbc755f82d03d
bearjun05 <bearjun05@naver.com>
2023-03-22 15:27:44 +0900

    feat: memberResponse 메시지 추가

--- src/main/java/com/blackdragon/heytossme/type/resposne/MemberResponse.java
+++ src/main/java/com/blackdragon/heytossme/type/resposne/MemberResponse.java
@@ -7,10 +7,11 @@ public enum MemberResponse {
     SIGN_UP("Successfully signed up"),
     FIND_INFO("successfully get user info"),
     CHANGE_INFO("successfully edit info"),
-
     DELETE_USER("successfully deleted"),
-
-    SIGN_IN("successfully signed up");
+    SIGN_IN("successfully signed up"),
+    SEND_EMAIL("successfully email sent"),
+    MATCH_CODE("successfully authorized"),
+    RESET_PASSWORD("successfully reset password");
 
     MemberResponse(String message) {
         this.message = message;
