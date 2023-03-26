f4cfcbe0462ef124e8427b50d754e0b3156f8da9
bearjun05 <bearjun05@naver.com>
2023-03-27 00:28:57 +0900

    refactor:  변수명 변경

--- src/main/java/com/blackdragon/heytossme/controller/UploadController.java
+++ src/main/java/com/blackdragon/heytossme/controller/UploadController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +23,7 @@ public class UploadController {
 
     @PostMapping
     public ResponseEntity<?> uploadImage(HttpServletRequest httpServletRequest,
-            Request request) {
+            @RequestBody Request request) {
 
         long id = (long) httpServletRequest.getAttribute("id");
 
