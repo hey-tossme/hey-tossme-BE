baeb8ef2b358819fe85c2bbfd481cdd3af6b8eb8
bearjun05 <bearjun05@naver.com>
2023-03-27 00:38:45 +0900

    feat: kakao value 키 추가

--- src/main/java/com/blackdragon/heytossme/controller/KakaoLoginController.java
+++ src/main/java/com/blackdragon/heytossme/controller/KakaoLoginController.java
@@ -5,6 +5,7 @@ import com.blackdragon.heytossme.service.KakaoLoginService;
 import com.blackdragon.heytossme.type.KakaoResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,21 +18,24 @@ import org.springframework.web.bind.annotation.RestController;
 @Log4j2
 public class KakaoLoginController {
 
-    private final String redirectUrl = "http://localhost:8080/login/oauth2/kakao";
-    private final String clientId = "5e7a808a1af99e1088e5d4eb40239800";
+    @Value("com.blackdragon.kakao.redirectUrl")
+    private String redirectUrl;
+    @Value("com.blackdragon.kakao.clientId")
+    private String clientId;
     private final KakaoLoginService kakaoLoginService;
 
+//    추후 삭제 예정
+//    @GetMapping()
+//    public String kakaoLogin() {
+//
+//        String url = "https://kauth.kakao.com/oauth/authorize?client_id=" + clientId
+//                + "&redirect_uri=" + redirectUrl +
+//                "&response_type=code";
+//
+//        return url;
+//    }
+
     @GetMapping("/login")
-    public String kakaoLogin() {
-
-        String url = "https://kauth.kakao.com/oauth/authorize?client_id=" + clientId
-                + "&redirect_uri=" + redirectUrl +
-                "&response_type=code";
-
-        return url;
-    }
-
-    @GetMapping("/callback")
     public ResponseEntity<?> oauthKakaoLogin(
             @RequestParam(value = "code", required = false) String code) {
 
