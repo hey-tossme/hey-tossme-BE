92fde9de339722c3e5a2757241cd57e2f9f4df34
bearjun05 <bearjun05@naver.com>
2023-03-27 22:36:36 +0900

    refactor: v1, v2 api 경로 추가

--- src/main/java/com/blackdragon/heytossme/controller/KakaoLoginController.java
+++ src/main/java/com/blackdragon/heytossme/controller/KakaoLoginController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/kakao")
+@RequestMapping("/v2/kakao")
 @RequiredArgsConstructor
 @Log4j2
 public class KakaoLoginController {
--- src/main/java/com/blackdragon/heytossme/controller/MemberController.java
+++ src/main/java/com/blackdragon/heytossme/controller/MemberController.java
@@ -26,26 +26,24 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 @RestController
-@RequestMapping("/members")
 @RequiredArgsConstructor
 @Slf4j
 public class MemberController {
 
     private final MemberService memberService;
 
-    @PostMapping
+    @PostMapping("/v2/members/")
     public ResponseEntity<ResponseForm> signUp(@Valid @RequestBody SignUpRequest request) {
         var data = memberService.signUp(request);
         return ResponseEntity.ok(new ResponseForm(MemberResponse.SIGN_UP.getMessage(), data));
     }
 
-    @PostMapping("/signIn")
+    @PostMapping("/v1/members/signIn")
     public ResponseEntity<?> signIn(@RequestBody SignInRequest request,
             HttpServletResponse response) {
 
@@ -68,7 +66,7 @@ public class MemberController {
     /**
      * Interceptor로부터 넘어오는 로그아웃 API
      */
-    @GetMapping("/logout/{accessToken}/{userId}")
+    @GetMapping("/v2/members/logout/{accessToken}/{userId}")
 //    public ResponseEntity<ResponseForm> logout( @PathVariable("accessToken") String token) {
     public ResponseEntity<ResponseForm> logout(@PathVariable("accessToken") String token,
             @PathVariable("userId") Object _userId) {
@@ -91,38 +89,38 @@ public class MemberController {
                 new ResponseForm(MemberResponse.SIGN_OUT.getMessage(), data, token));
     }
 
-    @GetMapping
+    @GetMapping("/v1/members")
     public ResponseEntity<ResponseForm> getInfo(HttpServletRequest httpServletRequest) {
-        long id = (long) httpServletRequest.getAttribute("id");
+        Long id = (Long) httpServletRequest.getAttribute("id");
         Response response = memberService.getInfo(id);
 
         return ResponseEntity.ok(
                 new ResponseForm(MemberResponse.FIND_INFO.getMessage(), response));
     }
 
-    @PatchMapping
+    @PatchMapping("/v1/members")
     public ResponseEntity<ResponseForm> modifyInfo(HttpServletRequest httpServletRequest,
             @Valid @RequestBody ModifyRequest request) {
 
-        long id = (long) httpServletRequest.getAttribute("id");
+        Long id = (Long) httpServletRequest.getAttribute("id");
         Response response = memberService.modifyInfo(id, request);
 
         return ResponseEntity.ok(
                 new ResponseForm(MemberResponse.CHANGE_INFO.getMessage(), response));
     }
 
-    @DeleteMapping
+    @DeleteMapping("/v1/members")
     public ResponseEntity<ResponseForm> delete(HttpServletRequest httpServletRequest,
             @Valid @RequestBody DeleteRequest request) {
 
-        long id = (long) httpServletRequest.getAttribute("id");
+        Long id = (Long) httpServletRequest.getAttribute("id");
         memberService.deleteUser(id, request);
 
         return ResponseEntity.ok(
                 new ResponseForm(MemberResponse.DELETE_USER.getMessage(), null));
     }
 
-    @PostMapping("reset-password")
+    @PostMapping("/v2/members/reset-password")
     public ResponseEntity<ResponseForm> sendResetMail(@Valid @RequestBody PasswordRequest request) {
 
         memberService.sendEmail(request);
@@ -131,7 +129,7 @@ public class MemberController {
                 new ResponseForm(MemberResponse.SEND_EMAIL.getMessage(), request));
     }
 
-    @PostMapping("reset-password/check")
+    @PostMapping("/v2/members/reset-password/check")
     public ResponseEntity<ResponseForm> checkAuthCode(@Valid @RequestBody PasswordRequest request) {
 
         memberService.checkAuthCode(request);
@@ -140,7 +138,7 @@ public class MemberController {
                 new ResponseForm(MemberResponse.MATCH_CODE.getMessage(), null));
     }
 
-    @PatchMapping("reset-password")
+    @PatchMapping("/v2/members/reset-password")
     public ResponseEntity<ResponseForm> resetNewPassword(
             @Valid @RequestBody PasswordRequest request) {
 
